38eb5cdb85c09c9bb9dac7ef371b5cce8b73c18d
tschneider35 <62909769+tschneider35@users.noreply.github.com>
2022-07-19 11:50:55 -0600

    Week 7 Stuff
    Week 7

---
+++ Week 5/index.js
@@ -0,0 +1,37 @@
+//initialize card deck as array
+class Deck{
+    constructor(royal, number, deck){
+        this.royal = royal;
+        this.number = number;
+        deck = ['A','K','Q','J','10','9','8','7','6','5','4','3','2'];
+        }
+}
+//create player and dealer classes
+class Player{
+    constructor(player){
+        this.player = player;
+    }
+}
+class Dealer{
+    constructor(dealer){
+        this.dealer = dealer;
+    }
+}
+
+//Initialize buttons
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
